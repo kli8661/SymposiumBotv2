cf533f5f348b2ce261bea2e7fad85017c772f0e2
BT_1N3_06 <kli8661@bths.edu>
2019-02-26 08:56:09 -0500

    Removed unneeded line.

--- testing.py
+++ testing.py
@@ -8,7 +8,6 @@ from discord.ext.commands import Bot
 TOKEN = 'NTQ1OTg0ODY4OTM3NjI5NzAw.D1SSSw.7QQpBnTcpERWKobvCn2J_zOZAAg'
 
 BOT_PREFIX = ("?", "!")
-TOKEN = "NTQ1OTg0ODY4OTM3NjI5NzAw.D1SSSw.7QQpBnTcpERWKobvCn2J_zOZAAg"
 
 client = Bot(command_prefix=BOT_PREFIX)
 

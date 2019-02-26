b9e789686cc18db375d4034595282ff0a5cfa4e8
BT_1N3_06 <kli8661@bths.edu>
2019-02-26 09:53:22 -0500

    Changed files

--- my_bot.py
+++ my_bot.py
@@ -7,6 +7,7 @@ import testing as ts
 TOKEN = 'NTQ1OTg0ODY4OTM3NjI5NzAw.D1SSSw.7QQpBnTcpERWKobvCn2J_zOZAAg'
 
 client = discord.Client()
+client.remove_command('help')
 
 
 @client.event
@@ -14,6 +15,20 @@ async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
+
+
+@client.command(pass_content=True)
+async def help(cmd):
+    author = cmd.message.author
+
+    embed = discord.Embed(
+        colour=discord.Colour.blue()
+    )
+
+    embed.set_author(name='Help')
+    embed.add_field(name='$ping', value='Returns pong', inline=False)
+
+    await client.send_message(author, embed=embed)
 
 ts.on_ready()
 ts.ping()

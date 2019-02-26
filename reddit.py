f337ae17af4c5a3ce9f638d0f1b856e01b4f7555
Elims <sjiang9453@bths.edu>
2019-02-26 09:33:16 -0500

    Commmit

--- reddit.py
+++ reddit.py
@@ -2,7 +2,7 @@ from discord.ext.commands import Bot
 
 TOKEN = 'NTQ1OTg0ODY4OTM3NjI5NzAw.D1SSSw.7QQpBnTcpERWKobvCn2J_zOZAAg'
 
-BOT_PREFIX = '#'
+BOT_PREFIX = '$'
 
 client = Bot(command_prefix=BOT_PREFIX)
 

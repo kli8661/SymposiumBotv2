73c385350cbb555266a823c01f1856437389a60d
Elims <sjiang9453@bths.edu>
2019-02-27 09:41:52 -0500

    Commmit

--- music.py
+++ music.py
@@ -1,10 +1,39 @@
-from discord.ext.commands import Bot
+from discord.ext import commands
 
 TOKEN = 'NTQ1OTg0ODY4OTM3NjI5NzAw.D1SSSw.7QQpBnTcpERWKobvCn2J_zOZAAg'
 
 BOT_PREFIX = '$'
 
-client = Bot(command_prefix=BOT_PREFIX)
+client = commands.Bot(command_prefix=BOT_PREFIX)
 
+
+@client.command(pass_context=True)
+async def join(msg):
+    join_channel = msg.message.server
+    await client .join_voice_channel(join_channel)
+
+
+@client.command(pass_context=True)
+async def leave(msg):
+    leave_channel = msg.message.server
+    voice_client = client.voice_client_in(leave_channel)
+    await voice_client.disconnect()
+
+
+@client.command(name='p',
+                description="plays an audio from youtube video",
+                brief="plays music",
+                aliases=['music', 'plays', 'audio'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely',
+        'Impossible',
+        'Try asking a better question'
+    ]
 
 client.run(TOKEN)

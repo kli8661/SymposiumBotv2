98725bd1abcba42391bc76df227f65dc922966be
BT_1N3_10 <kli8661@bths.edu>
2019-02-28 10:07:27 -0500

    added requirements.txt

--- reddit.py
+++ reddit.py
@@ -1,10 +1,47 @@
+# Kent Reddit Bot
+
+import json
+import http
 from discord.ext.commands import Bot
+
+urlprefix = "https://reddit.com/r/"
+urlsuffix = "/random.json"
+count = 0
 
 TOKEN = 'NTQ1OTg0ODY4OTM3NjI5NzAw.D1SSSw.7QQpBnTcpERWKobvCn2J_zOZAAg'
 
 BOT_PREFIX = '$'
 
 client = Bot(command_prefix=BOT_PREFIX)
+
+
+@client.command()
+async def rimage(sub):
+    while True:
+        subreddit = sub.content[7:]
+        subtest = urlprefix + subreddit + '.json'
+        response = http.request('GET', subtest)
+        check = response.data
+        parse = json.loads(check)
+        if parse['data']['dist'] == 0:
+            await client.send_message(sub.channel, 'That subreddit does not exist!')
+            return
+        url = urlprefix + subreddit + urlsuffix
+        while True:
+            response = http.request('GET', url)
+            jsonparse = json.loads(response.data.decode('utf-8'))
+            image = jsonparse[0]['data']['children'][0]['data']['url']
+            if image.endswith('.png') or image.endswith('jpeg') or image.endswith('jpg') or image.endswith('gif'):
+                await client.send_message(sub.channel, image)
+                return
+            else:
+                global count
+                count+=1
+            if count == 10:
+                await client.send_message(sub.channel, 'No image found.')
+                return
+            await client.send_message(sub.channel, 'Try command !rimage subreddit')
+            return
 
 
 client.run(TOKEN)
--- testing.py
+++ testing.py
@@ -4,14 +4,16 @@ import random
 import asyncio
 import aiohttp
 import json
+import http
 import discord
 from discord import Game
 from discord.ext.commands import Bot
 
+urlprefix = "https://reddit.com/r/"
+urlsuffix = "/random.json"
+count = 0
 TOKEN = 'NTQ1OTg0ODY4OTM3NjI5NzAw.D1SSSw.7QQpBnTcpERWKobvCn2J_zOZAAg'
-
 BOT_PREFIX = '$'
-
 client = Bot(command_prefix=BOT_PREFIX)
 
 
@@ -114,6 +116,38 @@ async def help_me(ctx):
     embed.add_field(name='$ping', value='Returns Pong!', inline=False)
 
     await client.send_message(author, embed=embed)
+
+
+@client.command(name='rimage',
+                description="Grabs image from user subreddit.",
+                brief="Grabs image from subreddit.",
+                pass_context=True)
+async def rimage(sub):
+    while True:
+        subreddit = sub.content[7:]
+        subtest = urlprefix + subreddit + '.json'
+        response = http.request('GET', subtest)
+        check = response.data
+        parse = json.loads(check)
+        if parse['data']['dist'] == 0:
+            await client.send_message(sub.channel, 'That subreddit does not exist!')
+            return
+        url = urlprefix + subreddit + urlsuffix
+        while True:
+            response = http.request('GET', url)
+            jsonparse = json.loads(response.data.decode('utf-8'))
+            image = jsonparse[0]['data']['children'][0]['data']['url']
+            if image.endswith('.png') or image.endswith('jpeg') or image.endswith('jpg') or image.endswith('gif'):
+                await client.send_message(sub.channel, image)
+                return
+            else:
+                global count
+                count+=1
+            if count == 10:
+                await client.send_message(sub.channel, 'No image found.')
+                return
+            await client.send_message(sub.channel, 'Try command !rimage subreddit')
+            return
 
 
 async def list_servers():

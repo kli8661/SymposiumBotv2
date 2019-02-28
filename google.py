91cb73302fd2b5c690bad028ac6fff16fc4fe44c
BT_1N3_08 <jn>
2019-02-28 10:10:24 -0500

    Test?

--- google.py
+++ google.py
@@ -7,12 +7,15 @@ TOKEN = 'NTQ1OTg0ODY4OTM3NjI5NzAw.D1SSSw.7QQpBnTcpERWKobvCn2J_zOZAAg'
 
 BOT_PREFIX = '$'
 api_key = 'AIzaSyATGAnmCuJHlvsdVn21472sJPuAiEanSY4'
-service = build('Discord', 'v1', developerKey=api_key)
+cx_key = '008921493878794350931:tioeliy7j1y'
+Customsearch = build('Discord', 'v1', developerKey=api_key)
 
-request = service.volumes().list(source='public', q='Test')
+request = Customsearch.Cse.Siterestrict.List(source='public', q='SEARCH ITEM HERE' )
 
 response = request.execute()
 pprint.pprint(response)
+
+
 
 
 client = Bot(command_prefix=BOT_PREFIX)

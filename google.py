bbcbf22ed8b8caff952810a1d7ea4d54abaf6fdd
BT_1N3_08 <jn>
2019-03-05 10:05:04 -0500

    Fix installing packages

--- google.py
+++ google.py
@@ -1,14 +1,13 @@
 import pprint
-import sys
-
-from discord.ext.commands import Bot
 from googleapiclient.discovery import build
+from discord.ext.commands import Bot
+
 
 TOKEN = 'NTQ1OTg0ODY4OTM3NjI5NzAw.D1SSSw.7QQpBnTcpERWKobvCn2J_zOZAAg'
-
 BOT_PREFIX = '.'
-api_key = 'AIzaSyATGAnmCuJHlvsdVn21472sJPuAiEanSY4'
-cx_key = '008921493878794350931:tioeliy7j1y'
+
+m_api_key = 'AIzaSyATGAnmCuJHlvsdVn21472sJPuAiEanSY4'
+m_cx_key = '008921493878794350931:tioeliy7j1y'
 
 
 def google_search(search_term, api_key, cx_id, **kwargs):
@@ -21,6 +20,13 @@ results = google_search('facebook', api_key, cx_key, num=2)
 
 for result in results:
     pprint.pprint(result)
+
+    title = result['title']
+    link = result['formattedUrl']
+    dis = result['snippet']
+    print(title)
+    print(link)
+    print(dis)
 
 client = Bot(command_prefix=BOT_PREFIX)
 

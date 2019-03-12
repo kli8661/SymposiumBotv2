6991b7fef034766b947f9c5e64254a42cc3ff092
BT_1N3_08 <jn>
2019-03-12 09:09:52 -0400

    changed reddit func

--- reddit.py
+++ reddit.py
@@ -20,20 +20,20 @@ reddit = praw.Reddit(client_id='y4QgcvDtchCuoA',
                 description='Grabs the top posts from a subreddit of the users choice.',
                 brief='Grabs top posts.',
                 pass_context=True)
-async def top_posts(ctx, post):
-    post = post
+async def top_posts(ctx, subreddit):
+    subreddit = subreddit
     channel = ctx.message.channel
-    post_sub = reddit.subreddit(post)
+    post_sub = reddit.subreddit(subreddit)
     hot_posts = post_sub.hot(limit=12)
     embed = discord.Embed(
-        author=post,
+        author=subreddit,
         title='Top Posts',
         colour=discord.Colour.blue()
     )
     for submission in hot_posts:
         if not submission.stickied:
-            embed.add_field(name=submission.title, value='Upvotes: ' +
-                            submission.ups + ', ' + 'Downvotes: ' + submission.downs, inline=False)
+            embed.add_field(name=str(submission.title), value='Upvotes: ' +
+                            str(submission.ups) + ', ' + 'Downvotes: ' + str(submission.downs), inline=False)
 
             await client.send_message(channel, embed=embed)
 

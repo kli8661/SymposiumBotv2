2b73eae7d498e92b09575d0e7c9762cf645fd60c
Kent <kli8661@bths.edu>
2019-03-12 17:13:07 -0400

    added rsearch command

--- testing.py
+++ testing.py
@@ -157,5 +157,24 @@ async def hot_posts(ctx, subreddit, amount):
     await client.send_message(channel, embed=embed)
 
 
+@client.command(name='rsearch',
+                description='Searches reddit.',
+                brief='Searches reddit. \n[.rsearch <query> <amount>]',
+                pass_context=True)
+async def rsearch(ctx, query, amount):
+    channel = ctx.message.channel
+    query = query
+    limit = int(amount)
+    sub = reddit.subreddit('all')
+    embed = discord.Embed(
+        author=str(query),
+        title='Search Results',
+        colour=discord.Colour.blue()
+    )
+    for i in sub.search(str(query), limit=limit):
+        embed.add_field(name=str(i) + str(i.title), value='URL: ' + str(i.url), inline=False)
+
+    await client.send_message(channel,embed=embed)
+
 client.loop.create_task(list_servers())
 client.run(TOKEN)

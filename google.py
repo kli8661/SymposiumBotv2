61ec974d1c8c216cbf4b427b62f427a41d7f2104
BT_1N3_08 <jn>
2019-03-20 10:09:54 -0400

    a

--- google.py
+++ google.py
@@ -1,8 +1,9 @@
 import json
 import urllib.parse
 import aiohttp
+from cogs.utils.checks import load_optional_config, embed_perms, get_google_entries
 from discord.ext.commands import Bot
-from discord import Game
+
 
 TOKEN = 'NTQ1OTg0ODY4OTM3NjI5NzAw.D2f2UA.AFTB7ougi3e3U0vytq7wUZ8RPIw'
 BOT_PREFIX = '.'
@@ -33,13 +34,21 @@ m_cx_key = '008921493878794350931:tioeliy7j1y'
                     pass_context=True)
 
     async def gsearch(ctx, *, query):
-        await ctx.message.delete
+        if not embed_perms(ctx.message):
+            config = load_optional_config()
         async with aiohttp.ClientSession() as session:
             async with session.get("https://www.googleapis.com/customsearch/v1?q=" + urllib.parse.quote_plus(query) + "&start=" + '1' + "&key=AIzaSyATGAnmCuJHlvsdVn21472sJPuAiEanSY4" + "&cx=008921493878794350931:tioeliy7j1y") as resp:
                 result = json.loads(await resp.txt())
-                return await ctx.send(result['items'][['link']]['snippet'])
+            return await ctx.send(result['items'][['link']]['snippet'])
+    try:
+        entries, root = await get_google_entries(query)
+        card_node = root.find(".//div[@id='topstuff']")
+        card = self.parse_google_card(card_node)
 
         # return return the snippet section of the links + websites
 
 client = Bot(command_prefix=BOT_PREFIX)
 client.run(TOKEN)
+
+
+

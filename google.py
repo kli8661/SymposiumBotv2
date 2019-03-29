6378782f88dbbf2d3bec4b0f5becc41c093b5c74
AlstonS <Ashi3454@bths.edu>
2019-03-29 00:48:44 -0400

    .

--- google.py
+++ google.py
@@ -1,8 +1,8 @@
 import discord
 import json
-from discord.ext import commands
+# from discord.ext import commands
 from cogs.utils.checks import load_optional_config, embed_perms, get_google_entries
-from cogs.utils.config import get_config_value
+# from cogs.utils.config import get_config_value
 import aiohttp
 import urllib.parse
 
